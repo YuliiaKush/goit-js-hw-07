636bbd1cc24875e8a96feee2dc79a27c64e0358c
YuliiaKush <128642386+YuliiaKush@users.noreply.github.com>
2023-08-01 13:11:17 +0300

    updateee

---
+++ js/01-gallery.js
@@ -0,0 +1,38 @@
+import { galleryItems } from './gallery-items.js';
+
+const container = document.querySelector('.gallery');
+
+
+function createImageListMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) =>
+        `<li class="gallery__item">
+        <a class="gallery__link" href="large-image.jpg">
+          <img  
+            class="gallery__image"
+            src="${preview}"
+            data-source="${original}"
+            alt="${description}"
+          />
+        </a>
+      </li>`).join('')
+
+}
+
+container.insertAdjacentHTML('beforeend', createImageListMarkup(galleryItems));
+container.addEventListener('click', handlerProductClick);
+
+function handlerProductClick(event) {
+    event.preventDefault();
+    const imgOriginalEl = event.target.dataset.source;
+    console.log(imgOriginalEl);
+    const instance = basicLightbox.create(`<img src = "${imgOriginalEl}">`);
+    instance.show();
+
+ }
+
+
+
+ 
+ 
+
+
